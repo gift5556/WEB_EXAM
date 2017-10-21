3b63584f5ef01070d58a6ade5c93d4f644fbf73d
Amornrat Thammajai <farmproject57@gmail.com>
2017-10-21 13:30:31 +0700

    add Home

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Home extends Component {
+  render(){
+    return(
+      <div>57022163 น.ส.อมรรัตน์ ธรรมใจ</div>
+    )
+  }
+}
+export default Home;
