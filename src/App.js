3a2080e38777a329b3eed0e14c2858bd6337738b
Amornrat Thammajai <farmproject57@gmail.com>
2017-10-21 15:29:25 +0700

    2 commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
+import Home2 from './Home2';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className="App">
+            <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <ul>
+                    <li><NavLink to="/Home" activeClassName="active">Home</NavLink></li>
+                    <li><NavLink to="/Home2" activeClassName="active">Home2</NavLink></li>
+                </ul>
+            </div>
+
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <Home/>
-        </p>
+
+        <div className="App-intro">
+            <Home/>
+
+            <button onClick> Get Data </button>
+            <button onClick> Clear </button>
+        </div>
       </div>
+
+
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import Home2 from './Home2';
+
+
+const reducer = (state , action) =>{
+  switch (action.type) {
+    case "ADD":
+      state+=action.payload;
+      break;
+    case "SUB":
+      state-=action.payload;
+      break;
+    default:
+
+  }
+  return state;
+}
+const store = createStore(reducer,15000);
+store.subscribe(()=>{
+  console.log("Update Store",store.getState());
+})
+store.dispatch({
+  type:"ADD",
+  payload:500
+});
+store.dispatch({
+  type:"SUB",
+  payload:500
+});
+
+store.subscribe(() => {console.log("store change : " + store.getState()); });
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
